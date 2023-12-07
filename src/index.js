0207ac595e84eb43b2a005f2b16e79af3e6edf62
vicheath <vicheath.a.s@gmail.com>
2023-12-07 18:00:36 -0500

    lab incomplete

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import "../src/style.css";
 
+const body = document.querySelector("body");
+const mainEl = document.querySelector("main");
+const mainBgColor = getComputedStyle(body).getPropertyValue("--main-bg");
+mainEl.style.backgroundColor = mainBgColor;
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+
+const topMenuBgColor = getComputedStyle(body).getPropertyValue("--top-menu-bg");
+topMenuEl.style.backgroundColor = topMenuBgColor;
+topMenuEl.classList.add("flex-around");
+
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+menuLinks.forEach((link) => {
+  const linkElement = document.createElement("a");
+  linkElement.href = link.href;
+  linkElement.textContent = link.text;
+  topMenuEl.appendChild(linkElement);
+});
